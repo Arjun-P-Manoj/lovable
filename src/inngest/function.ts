54f86a1924c43347c905f130b7aae7a842838ce1
Arjun-P-Manoj <arjunpmanoj1@gmail.com>
2025-12-31 15:02:31 +0530

    started a sandbox in inngest with new template

--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,5 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 import { inngest } from "./client";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY!,
@@ -9,16 +11,20 @@ export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    console.log("Gemini key loaded:", !!process.env.GEMINI_API_KEY);
+    // console.log("Gemini key loaded:", !!process.env.GEMINI_API_KEY);
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("lovable-clone-testv2");
+      return sandbox.sandboxId;
+    });
 
     const userInput = event.data.value;
 
     const resultText = await step.run("generate-gemini-output", async () => {
       const prompt = `
-You are an expert next.js developer.You write readable maintainable code.you write simple Next.js & React snippets
-
-User input:Write the following snippet
-"${userInput}"
+        You are an expert next.js developer.You write readable maintainable code.you write simple Next.js & React snippets.
+        Return ONLY code (no explanations)
+        User input:Write the following snippet
+        "${userInput}"
 
 `;
 
@@ -29,8 +35,14 @@ User input:Write the following snippet
 
       return result.text;
     });
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
     console.log("Gemini summary:", resultText);
 
-    return resultText;
+    return {resultText,sandboxUrl};
   }
 );
